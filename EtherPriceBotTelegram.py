af9d81276abf8cf20f0cf2923b5c45a027b4a437
markbala <mark@bala.sg>
2018-03-25 23:58:03 +0800

    bug fixes
    bug fixes

--- EtherPriceBotTelegram.py
+++ EtherPriceBotTelegram.py
@@ -36,7 +36,7 @@ class TelegramBot(object):
         return js
 
     def _getUpdates(self, offset=None):
-        url = self.URL+"getUpdates?timeout=100"
+        url = self.URL+"getUpdates?timeout=60"
         if offset:
             url += "&offset={}".format(offset)
         js = self._getJsonFromURL(url)
@@ -64,35 +64,30 @@ class TelegramBot(object):
         self.lastUpdateId = None
         while True:
             self.updates = self._getUpdates(self.lastUpdateId)
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+":"+str(self.updates))
             if len(self.updates["result"]) > 0:
                 self.lastUpdateId = self.getLastUpdateId(self.updates) + 1
                 self.executionFunction(self.updates)
-            time.sleep(0.5)
 
     def executionFunction(self,updates):
         for update in updates["result"]:
-            try:
-                text = update["message"]["text"]
-                chat = update["message"]["chat"]["id"]
-                if text == "/ether":
-                        ethkrw = self._getJsonFromURL('https://api.korbit.co.kr/v1/ticker?currency_pair=eth_krw')
-                        ethsgd = self._getJsonFromURL('https://coinhako.com/api/v1/price/currency/ETHSGD')
-                        ethsgd = ethsgd['data']['buy_price']
-                        message1 = "Buy ETH on Coinhako S$ at "+str(ethsgd)
-                        ETHspread = (float(ethkrw["last"])/816.94 - float(ethsgd))/float(ethsgd)
-                        ETHspreadPercentage = "{0:.2f}%".format(ETHspread*100)
-                        message2 = str(ETHspreadPercentage)+" spread to Korbit, which trades at "+str("{0:.2f}".format(float(ethkrw["last"])/816.94))
-                        print(message1+"\n"+message2)
-                        self.sendMessage(message1+"\n"+message2,chat)
-                elif text == '/help':
-                    self.sendMessage("'Welcome to Mark's Ether Price Tracking BOT\n\
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            if text == "/ether":
+                    ethkrw = self._getJsonFromURL('https://api.korbit.co.kr/v1/ticker?currency_pair=eth_krw')
+                    ethsgd = self._getJsonFromURL('https://coinhako.com/api/v1/price/currency/ETHSGD')
+                    ethsgd = ethsgd['data']['buy_price']
+                    message1 = "Buy ETH on Coinhako S$ at "+str(ethsgd)
+                    ETHspread = (float(ethkrw["last"])/816.94 - float(ethsgd))/float(ethsgd)
+                    ETHspreadPercentage = "{0:.2f}%".format(ETHspread*100)
+                    message2 = str(ETHspreadPercentage)+" spread to Korbit, which trades at "+str("{0:.2f}".format(float(ethkrw["last"])/816.94))
+                    print(message1+"\n"+message2)
+                    self.sendMessage(message1+"\n"+message2,chat)
+            elif text == '/help':
+                self.sendMessage("'Welcome to Mark's Ether Price Tracking BOT\n\
 '/ether' to retrieve current prices and spread", chat)
 
-            except Exception as e:
-                print(str(e))
-                pass
 
-q = 0
 while True:
     try:
         print('BOT Started')
@@ -101,6 +96,5 @@ while True:
     except Exception as e:
         print(e)
         del TelegramBOT
-        time.sleep(10)
-        q += 1
-        print('Restarting in 10 secs. exception: '+str(q))
+        time.sleep(5)
+        print('Restarting in 5 seconds. Error at: '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
